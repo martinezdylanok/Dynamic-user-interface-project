cc39d4c583d448aea2f0d4e9b99b812007f999da
Lpik97 <dm97fx@gmail.com>
2023-08-09 18:26:11 +0200

    feat(hamburguer menu display function imported and invoked)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
+import HAMBURGUER_MENU_DISPLAY from "./js_modules/hamburguermenudisplay.js";
 
 CHANGE_ELEMENT_STATE();
+HAMBURGUER_MENU_DISPLAY();
