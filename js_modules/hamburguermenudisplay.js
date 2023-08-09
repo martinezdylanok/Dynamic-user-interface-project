8b2c3af6c2c72a8918a74980beb3726098667917
Lpik97 <dm97fx@gmail.com>
2023-08-09 18:22:12 +0200

    feat(hamburguer menu display function module)
    This module is based on the changeelementstatus, the only change is that the element selected will be menu button.

---
+++ js_modules/hamburguermenudisplay.js
@@ -0,0 +1,24 @@
+const HAMBURGUER_MENU_DISPLAY = () => {
+  const MENU_BUTTON = document.querySelector(".another-drop-down-button");
+
+  const BODY = document.querySelector("body");
+
+  MENU_BUTTON.addEventListener("mouseover", () => {
+    MENU_BUTTON.classList.add("hovered");
+    MENU_BUTTON.addEventListener("mouseout", () => {
+      MENU_BUTTON.classList.remove("hovered");
+    });
+  });
+
+  MENU_BUTTON.addEventListener("click", () => {
+    MENU_BUTTON.classList.add("clicked");
+  });
+
+  BODY.addEventListener("click", (event) => {
+    if (event.target !== MENU_BUTTON) {
+      MENU_BUTTON.classList.remove("clicked");
+    }
+  });
+};
+
+export default HAMBURGUER_MENU_DISPLAY;
