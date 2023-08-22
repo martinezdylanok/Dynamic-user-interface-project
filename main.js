ea16918eafbd7d26795414bcb94eee21df536646
Lpik97 <dm97fx@gmail.com>
2023-08-22 12:15:36 +0200

    feat(changeimagecarouselstatus function invoked)

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
+import CHANGE_IMAGE_CAROUSEL_STATUS from "./js_modules/changeimagecarouselstatus.js";
 import HAMBURGUER_MENU_DISPLAY from "./js_modules/hamburguermenudisplay.js";
 
 CHANGE_ELEMENT_STATE();
 HAMBURGUER_MENU_DISPLAY();
+CHANGE_IMAGE_CAROUSEL_STATUS();
