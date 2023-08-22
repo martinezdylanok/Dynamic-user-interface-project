a805fc0814a7ebaa5aa4063b296fe2c05054b8b6
Lpik97 <dm97fx@gmail.com>
2023-08-22 13:53:41 +0200

    feat(display img function invoked)

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
 import CHANGE_IMAGE_CAROUSEL_STATUS from "./js_modules/changeimagecarouselstatus.js";
+import DISPLAY_IMG from "./js_modules/displayimage.js";
 import HAMBURGUER_MENU_DISPLAY from "./js_modules/hamburguermenudisplay.js";
 
 CHANGE_ELEMENT_STATE();
 HAMBURGUER_MENU_DISPLAY();
 CHANGE_IMAGE_CAROUSEL_STATUS();
+DISPLAY_IMG();
