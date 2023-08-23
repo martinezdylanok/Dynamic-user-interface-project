01c09e358ee104a7f3a108a5a4fedbee0766cd5b
Lpik97 <dm97fx@gmail.com>
2023-08-23 08:44:44 +0200

    feat(change image function imported and invoked, display image deleted)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
+import CHANGE_IMAGE from "./js_modules/changeimage.js";
 import CHANGE_IMAGE_CAROUSEL_STATUS from "./js_modules/changeimagecarouselstatus.js";
-import DISPLAY_IMG from "./js_modules/displayimage.js";
 import HAMBURGUER_MENU_DISPLAY from "./js_modules/hamburguermenudisplay.js";
 
 CHANGE_ELEMENT_STATE();
 HAMBURGUER_MENU_DISPLAY();
 CHANGE_IMAGE_CAROUSEL_STATUS();
-DISPLAY_IMG();
+CHANGE_IMAGE();
