6064d0c1ed2a080f323e4182fff2d48dac15bdc3
Lpik97 <dm97fx@gmail.com>
2023-08-23 08:44:17 +0200

    fix(currentIndex parameter)
    The currentIndex will be used as a parameter to check if the current image in the IMAGES array match its value. If it does, then the displayed classlist is added to the image.

--- js_modules/displayimage.js
+++ js_modules/displayimage.js
@@ -1,24 +1,11 @@
 import REMOVE_IMAGE_CLASS from "./removeimageclass.js";
 
-const DISPLAY_IMG = () => {
+const DISPLAY_IMG = (currentIndex) => {
   const IMAGES = document.querySelectorAll("img");
+  REMOVE_IMAGE_CLASS();
 
-  if (IMAGES[0]) {
-    const IMAGE01 = document.getElementById("image-1");
-    REMOVE_IMAGE_CLASS();
-    IMAGE01.classList.add("displayed");
-  } else if (IMAGES[1]) {
-    const IMAGE02 = document.getElementById("image-02");
-    REMOVE_IMAGE_CLASS();
-    IMAGE02.classList.add("displayed");
-  } else if (IMAGES[2]) {
-    const IMAGE03 = document.getElementById("image-03");
-    REMOVE_IMAGE_CLASS();
-    IMAGE03.classList.add("displayed");
-  } else {
-    const IMAGE04 = document.getElementById("image-04");
-    REMOVE_IMAGE_CLASS();
-    IMAGE04.classList.add("displayed");
+  if (IMAGES[currentIndex]) {
+    IMAGES[currentIndex].classList.add("displayed");
   }
 };
 
