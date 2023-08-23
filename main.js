63c15559d5582bbe0e7de40896d52170acf8cf79
Lpik97 <dm97fx@gmail.com>
2023-08-23 10:10:16 +0200

    feat(setInterval using auto move forward function)
    An interval was set with 5000 ms to call the auto move forward function.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import AUTO_MOVE_FORWARD from "./js_modules/automoveforward.js";
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
 import CHANGE_IMAGE from "./js_modules/changeimage.js";
 import CHANGE_IMAGE_CAROUSEL_STATUS from "./js_modules/changeimagecarouselstatus.js";
@@ -7,3 +8,4 @@ CHANGE_ELEMENT_STATE();
 HAMBURGUER_MENU_DISPLAY();
 CHANGE_IMAGE_CAROUSEL_STATUS();
 CHANGE_IMAGE();
+setInterval(AUTO_MOVE_FORWARD, 5000);
