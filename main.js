c81b969f9ee915cfaac0155b6bcaa5ebf7643e8a
Lpik97 <dm97fx@gmail.com>
2023-08-24 07:31:14 +0200

    feat(change navigation dot status function imported and invoked)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import AUTO_MOVE_FORWARD from "./js_modules/automoveforward.js";
 import CHANGE_ELEMENT_STATE from "./js_modules/changeelementstate.js";
 import CHANGE_IMAGE from "./js_modules/changeimage.js";
 import CHANGE_IMAGE_CAROUSEL_STATUS from "./js_modules/changeimagecarouselstatus.js";
+import CHANGE_NAVIGATION_DOT_STATE from "./js_modules/changenavigationdotstatus.js";
 import HAMBURGUER_MENU_DISPLAY from "./js_modules/hamburguermenudisplay.js";
 
 CHANGE_ELEMENT_STATE();
@@ -9,3 +10,4 @@ HAMBURGUER_MENU_DISPLAY();
 CHANGE_IMAGE_CAROUSEL_STATUS();
 CHANGE_IMAGE();
 setInterval(AUTO_MOVE_FORWARD, 5000);
+CHANGE_NAVIGATION_DOT_STATE();
