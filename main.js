eff0810a9e3b894682844cee142ec3932d3ff186
Lpik97 <dm97fx@gmail.com>
2023-08-26 09:01:12 +0200

    fix(interval removed from the function)

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ import CHANGE_ANOTHER_DROP_DOWN_BUTTON_STATE from "./js_modules/changeAnotherDro
 import CHANGE_IMAGE_CAROUSEL_BUTTON_STATE from "./js_modules/changeImageCarouselButtonState.js";
 import CHANGE_NAVIGATION_DOT_STATE from "./js_modules/changeNavDotState.js";
 import CHANGE_IMAGE from "./js_modules/changeImage.js";
-import AUTO_MOVE_FORWARD from "./js_modules/autoMoveForward.js";
 
 CHANGE_ELEMENT_STATE();
 CHANGE_ANOTHER_DROP_DOWN_BUTTON_STATE();
 CHANGE_IMAGE_CAROUSEL_BUTTON_STATE();
 CHANGE_NAVIGATION_DOT_STATE();
 CHANGE_IMAGE();
-setInterval(AUTO_MOVE_FORWARD, 5000);
