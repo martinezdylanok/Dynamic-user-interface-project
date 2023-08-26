12f78ea2168588c0c37ab03b810e2f3ab7761996
Lpik97 <dm97fx@gmail.com>
2023-08-26 08:01:02 +0200

    feat(remove dot id imported, change in function properties)
    The remove dot id function was imported and invoked. The function now checks also for the DOTS current index element, changing its id to focused-dot.

--- js_modules/imageDisplay.js
+++ js_modules/imageDisplay.js
@@ -1,10 +1,13 @@
+import REMOVE_DOT_ID from "./removeDotId.js";
 import REMOVE_IMAGE_CLASS from "./removeImageClass.js";
 
-const IMAGE_DISPLAY = (currentIndex, IMAGES) => {
+const IMAGE_DISPLAY = (currentIndex, IMAGES, DOTS) => {
   REMOVE_IMAGE_CLASS();
+  REMOVE_DOT_ID();
 
-  if (IMAGES[currentIndex]) {
+  if (IMAGES[currentIndex] && DOTS[currentIndex]) {
     IMAGES[currentIndex].classList.add("displayed");
+    DOTS[currentIndex].id = "focused-dot";
   }
 };
 
